a37003856a36eef491c21c885775c8fc1d50d30b
azer <benazzouzaze@gmail.com>
2024-06-10 01:59:42 +0100

    chore: Update prompt route to sort prompts by creation date

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const  GET = async (req) => {
     try{
         await connectToDatabase();
         
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find({}).populate('creator').sort({ createdAt: 1 });
         return new Response(JSON.stringify(prompts),{
             status: 200
         });
@@ -28,7 +28,7 @@ export const  POST = async (req) => {
                 {prompt:{$regex: searchText, $options: 'i'}},
                 {tag:{$regex: searchText, $options: 'i'}}
             ]
-        }).populate('creator');
+        }).populate('creator').sort({ createdAt: -1 });
 
         return new Response(JSON.stringify(prompts),{
             status: 200
